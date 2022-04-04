57f7060cf08cd8da4af0514ff22e368609a98b64
Jack Sutton <jack.sutton@madetech.com>
2022-04-04 13:03:02 +0100

    add content to startpage

--- src/Components/StartPage.js
+++ src/Components/StartPage.js
@@ -1,13 +1,60 @@
 import React from 'react';
 import Header from './Header';
 import Menu from './Menu';
+import { Breadcrumbs, Heading, GridCol, GridRow, LeadParagraph, InsetText, Button, ButtonArrow } from 'govuk-react';
 
 
 const StartPage = () => {
     return (
         <div className='start-page'>
-            Content
+          <Breadcrumbs>
+            <Breadcrumbs.Link href="#">
+              Section
+            </Breadcrumbs.Link>
+            <Breadcrumbs.Link href="#">
+              Sub-section
+            </Breadcrumbs.Link>
+            Current page
+          </Breadcrumbs>
+
+
+          <GridRow>
+            <GridCol setWidth="two-thirds">
+              <Heading size="XLARGE">
+                Book an appointment with Skills For Care
+              </Heading>
+            </GridCol>
+          </GridRow>
           
+
+          <GridRow>
+            <GridCol setWidth="two-thirds">
+              <div>
+              <LeadParagraph>
+                Use this service to book a consultation with one of our Skills For Care counsellor.
+              </LeadParagraph>
+              <InsetText>
+                You will need your payment details if applying for paid service.
+              </InsetText>
+              
+              <Button
+                icon={<ButtonArrow />}
+                start
+              >
+                Start now
+              </Button>
+
+              </div>
+            </GridCol>
+            <GridCol setWidth="one-third">
+              <div>
+                content
+              </div>
+            </GridCol>
+          </GridRow>
+
+
+
         </div>
     );
 };
