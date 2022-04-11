a27af6fa7b347e06c8416ce65999f2ad7dd18928
Joe Fong <joe.fong@madetech.com>
2022-04-11 14:10:27 +0100

    remove unused name/date/type states and link to checkDetail from App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
-import Header from './Components/Header'
-import Confirmation from './Components/Confirmation';
-import CheckDetails from './Components/CheckDetails';
-import Menu from './Components/Menu'
-import Name from './Components/Name'
-import BookingForm from './Components/BookingForm';
-import AppointmentType from './Components/AppointmentType';
-import AppointmentDate from './Components/AppointmentDate';
-import './App.css';
-import React, { useState } from 'react';
-import StartPage from './Components/StartPage';
 import {
   BrowserRouter,
   Routes,
   Route,
-  Link
 } from "react-router-dom";
 
+import './App.css';
 
-
+import Header from './Components/Header'
+import Menu from './Components/Menu'
+import BookingForm from './Components/BookingForm';
+import StartPage from './Components/StartPage';
 
 function App() {
-
-  const [name, setName] = useState("");
-  const [type, setType] = useState("");
-  const [date, setDate] = useState("");
 
   return (
 
@@ -36,10 +24,6 @@ function App() {
         <div className='content-column-2'>
             <Routes>
             <Route path="/" element={<StartPage />} />
-            {/* <Route path="/name" element={<Name name={name} setName={setName}/>} />
-            <Route path="/type" element={<AppointmentType type={type} setType={setType}/>} />
-            <Route path="/date" element={<AppointmentDate date={date} setDate={setDate} type={type}/>} /> */}
-            <Route path="/checkdetails" element={<CheckDetails name={name} date={date} type={type}/>} />
             <Route path="/bookingform/*" element={<BookingForm/>} />
             
             </Routes>
