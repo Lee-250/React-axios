07e89e23b7f937c353149005076b9abc8e791c2e
Joe Fong <joe.fong@madetech.com>
2022-04-13 12:11:24 +0100

    change date state internal value to format YYYY-MM-DDTHH:mm

--- src/Components/InputAppointmentDate.js
+++ src/Components/InputAppointmentDate.js
@@ -1,35 +1,40 @@
-import React from 'react';
-import { Select } from 'govuk-react';
+import React from "react";
+import { Select } from "govuk-react";
+import { display_date_to_user } from '../utils/datetime';
 
-const InputAppointmentDate = ({date, setDate}) => {
-    return (
-        <div>
-            <Select
-                input={{
-                    name: 'group2',
-                    onChange: (e) => {
-                        setDate(e.target.value) 
-                        console.log("called on change")
-                      },
-                      required: true, value: date
-                }}
-                label="Appointment Date and Time"
-                >
-                <option value="" disabled>
-                  Please choose from below
-                </option>
-                <option value="Monday 20th 2:00 pm">
-                  Monday 20th 2:00 pm
-                </option>
-                <option value="Tuesday 21st 3:00 pm">
-                  Tuesday 21st 3:00 pm
-                </option>
-                <option value="Wednesday 22nd 4:00 pm">
-                  Wednesday 22nd 4:00 pm
-                </option>
-            </Select>
-        </div>
-    );
+const defaultDateChoices = [
+  "2022-04-25T14:00",
+  "2022-04-26T15:00",
+  "2022-04-27T16:00",
+];
+
+const InputAppointmentDate = ({ date, setDate }) => {
+  const date_choices = defaultDateChoices;
+  return (
+    <div>
+      <Select
+        input={{
+          name: "date_and_time",
+          onChange: (e) => {
+            setDate(e.target.value);
+          },
+          required: true,
+          value: date,
+        }}
+        label="Appointment Date and Time"
+      >
+        <option value="" disabled>
+          Please choose from below
+        </option>
+        {date_choices.map(date_value => (
+          <option value={date_value}>{display_date_to_user(date_value)}</option>
+        ))}
+        {/* <option value="Monday 20th 2:00 pm">Monday 20th 2:00 pm</option>
+        <option value="Tuesday 21st 3:00 pm">Tuesday 21st 3:00 pm</option>
+        <option value="Wednesday 22nd 4:00 pm">Wednesday 22nd 4:00 pm</option> */}
+      </Select>
+    </div>
+  );
 };
 
 export default InputAppointmentDate;
