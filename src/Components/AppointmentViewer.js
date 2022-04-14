6c79c5fd5ef853ab3905a635825c6e7ab9c69f1b
Jack Sutton <jack.sutton@madetech.com>
2022-04-14 14:55:19 +0100

    appointment viewer added

--- src/Components/AppointmentViewer.js
+++ src/Components/AppointmentViewer.js
@@ -1,3 +1,4 @@
+
 import * as React from 'react';
 import Paper from '@mui/material/Paper';
 import { ViewState } from '@devexpress/dx-react-scheduler';
@@ -6,27 +7,36 @@ import {
   DayView,
   Appointments,
 } from '@devexpress/dx-react-scheduler-material-ui';
+import datePicker from './DatePicker';
 
-const currentDate = '2022-04-12';
-const appointments = [
-  { title: 'Training', startDate: '2022-04-12T10:00' },
-  { title: 'Apprenticeship', startDate: '2022-04-12T10:30', endDate: '2022-04-12T11:30' },
-  { title: 'Consultation', startDate: '2022-04-12T12:35' },
-];
+const AppointmentViewer = () => {
 
-export default () => (
-  <Paper>
-    <Scheduler
-      data={appointments}
-    >
-      <ViewState
-        currentDate={currentDate}
-      />
-      <DayView
-        startDayHour={9.5}
-        endDayHour={13.5}
-      />
-      <Appointments />
-    </Scheduler>
-  </Paper>
-);
+    const currentDate = '2022-04-12';
+    const appointments = [
+      { title: 'Training', startDate: '2022-04-12T10:00' },
+      { title: 'Apprenticeship', startDate: '2022-04-12T10:30', endDate: '2022-04-12T11:30' },
+      { title: 'Consultation', startDate: '2022-04-12T12:35' },
+    ];
+    return (
+        <div>
+            <datePicker/>
+            <Paper>
+                <Scheduler
+                data={appointments}
+                >
+                <ViewState
+                    currentDate={currentDate}
+                />
+                <DayView
+                    startDayHour={9.5}
+                    endDayHour={13.5}
+                />
+                <Appointments />
+                </Scheduler>
+            </Paper>
+
+        </div>
+    );
+};
+
+export default AppointmentViewer;
