888b7206ce1a6aa6c98f7b636bd1c113c491a1fd
Joe Fong <joe.fong@madetech.com>
2022-04-19 11:35:00 +0100

    tidy up code of calendar component and add today button

--- src/Components/Calendar.js
+++ src/Components/Calendar.js
@@ -9,64 +9,14 @@ import {
   AppointmentTooltip,
   DateNavigator,
   Toolbar,
+  TodayButton,
   ViewSwitcher,
 } from "@devexpress/dx-react-scheduler-material-ui";
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 import moment from "moment";
-import axios from 'axios';
+import axios from "axios";
 
-const apiUrl = "https://skillsforcare-api.herokuapp.com"   
-
-// const appointment_data_from_backend = [
-//     {
-//       id: 1,
-//       name: "Alpaca",
-//       email: "llama@kyle.com",
-//       type: "Consultation",
-//       date: "2022-04-11T12:00",
-//       created_at: "2022-04-11T10:43:28.488538Z",
-//     },
-//     {
-//       id: 2,
-//       name: "2nd Alpaca",
-//       email: "llama@kyle.com",
-//       type: "Consultation",
-//       date: "2022-04-11T14:00",
-//       created_at: "2022-04-11T10:51:40.466445Z",
-//     },
-//     {
-//       id: 3,
-//       name: "Lee",
-//       email: "e.e@e.com",
-//       type: "Consultation",
-//       date: "2022-04-12T10:00",
-//       created_at: "2022-04-11T10:51:57.462887Z",
-//     },
-//     {
-//       id: 4,
-//       name: "Test",
-//       email: "tedbooton@gmail.com",
-//       type: "Apprenticeships",
-//       date: "2022-04-14T12:00",
-//       created_at: "2022-04-11T11:20:48.462865Z",
-//     },
-//     {
-//       id: 5,
-//       name: "Lee",
-//       email: "e.e@e.com",
-//       type: "Consultation",
-//       date: "2022-04-14T15:30",
-//       created_at: "2022-04-11T11:20:58.463284Z",
-//     },
-//     {
-//       id: 6,
-//       name: "Kyle",
-//       email: "kyle@llama.com",
-//       type: "Training",
-//       date: "2022-04-13T11:00",
-//       created_at: "2022-04-13T11:19:11.034973Z",
-//     },
-// ];
+const apiUrl = "https://skillsforcare-api.herokuapp.com";
 
 const mapAppointmentData = (appointment) => ({
   id: appointment.id,
@@ -75,33 +25,8 @@ const mapAppointmentData = (appointment) => ({
   title: `${appointment.type} booked by ${appointment.name}`,
   type: appointment.type,
   name: appointment.name,
-  email: appointment.email
+  email: appointment.email,
 });
-
-
-const appointment_data = [
-  {
-    title: "Consultation booked by Kyle",
-    name: "Kyle",
-    type: "Consultation",
-    startDate: "2022-04-13T10:00",
-    endDate: "2022-04-13T12:00",
-  },
-  {
-    title: "Alpaca brushing session booked by Jack",
-    name: "Jack",
-    type: "Alpaca brushing",
-    startDate: "2022-04-13T13:30",
-    endDate: "2022-04-13T15:00",
-  },
-  {
-    title: "Training session booked by Dom",
-    name: "Dom",
-    type: "Training",
-    startDate: "2022-04-15T11:00",
-    endDate: "2022-04-15T13:00",
-  },
-];
 
 const get_custom_style = (appointment_type) => {
   switch (appointment_type) {
@@ -133,24 +58,21 @@ const CustomAppointment = ({ children, style, ...restProps }) => {
 };
 
 const Calendar = () => {
-  
   const [data, setData] = useState([]);
-  // const [data, useData] = useState(appointment_data_from_backend.map(mapAppointmentData));
   useEffect(() => {
-    axios.get(`${apiUrl}/api/appointment`)
-    .then(response_from_api => {
-      console.log('response here ---->', response_from_api)
+    axios
+      .get(`${apiUrl}/api/appointment`)
+      .then((response_from_api) => {
+        const data_for_calendar = response_from_api.data.map((data) =>
+          mapAppointmentData(data)
+        );
+        setData(data_for_calendar);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-  const data_for_calendar = response_from_api.data.map(data => mapAppointmentData(data))
-       setData(data_for_calendar)
-    })
-    .catch(err => {
-      console.log(err)
-    })
-  }, []);  
-  
-  // console.log(appointment_data_from_backend.map(data => mapAppointmentData(data)), "<---")
-  
   return (
     <Paper>
       <Scheduler data={data} height={660}>
@@ -164,13 +86,11 @@ const Calendar = () => {
         <MonthView />
 
         <Toolbar />
+        <TodayButton />
         <ViewSwitcher />
         <Appointments appointmentComponent={CustomAppointment} />
-        <AppointmentTooltip showCloseButton showOpenButton />
+        <AppointmentTooltip showCloseButton />
         <DateNavigator />
-        {/* <AppointmentForm
-          readOnly
-        /> */}
       </Scheduler>
     </Paper>
   );
